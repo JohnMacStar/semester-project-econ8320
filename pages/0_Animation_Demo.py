cc353d1a160819fd829ba9e883c53f66504f7bd2
JohnMacStar <157549072+JohnMacStar@users.noreply.github.com>
2024-05-06 14:58:07 -0500

    Update 0_Animation_Demo.py

--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -19,68 +19,59 @@ import numpy as np
 import streamlit as st
 from streamlit.hello.utils import show_code
 
-def animation_demo() -> None:
+def Page2() -> None:
+    data = pd.read_csv("https://github.com/JohnMacStar/semester-project-econ8320/releases/download/Data/ECON8320Final.csv")
+    #st.dataframe(data.head())
+    sample2024 = data[data['Year'] == 2024]
+    sample2024['PEEDUCA'] = sample2024['PEEDUCA'].str.replace('(ged)','')
+    sample2024['PEEDUCA'] = sample2024['PEEDUCA'].str.replace('(ex:ba,ab,bs)','')
+    sample2024['PEEDUCA'] = sample2024['PEEDUCA'].str.replace("(EX:MA,MS,MEng,MEd,MSW)",'')
+    
+    test = sample2024.HEFAMINC.str.extract(r'(\d+,?\d+)?([^0-9]*)?(\d+?,\d+)?')
+    test[0] = test[0].str.replace(',','')
+    test[0] = test[0].fillna("0")
+    test[0] = test[0].astype(int)
+    ulttest = pd.concat([test,sample2024], axis = 1)
+    ulttest = ulttest.rename(columns = {0:"Income"})
+    
+    kiddata = sample2024[['PEEDUCA', 'PRNMCHLD']]
+    kiddata = kiddata.groupby(['PEEDUCA']).mean().reset_index()
 
-    # Interactive Streamlit elements, like these sliders, return their value.
-    # This gives you an extremely simple interaction model.
-    iterations = st.sidebar.slider("Level of detail", 2, 20, 10, 1)
-    separation = st.sidebar.slider("Separation", 0.7, 2.0, 0.7885)
+    incvchld = ulttest[['Income', 'PRNMCHLD']]
+    incvchld = incvchld.groupby(["Income"]).mean().reset_index()
 
-    # Non-interactive elements return a placeholder to their location
-    # in the app. Here we're storing progress_bar to update it later.
-    progress_bar = st.sidebar.progress(0)
+    raceinc = ulttest[['Income', 'PTDTRACE']]
+    raceinc = raceinc.groupby(["PTDTRACE"]).mean().reset_index()
+    raceinc = raceinc.sort_values('Income', ascending=False)
 
-    # These two elements will be filled in later, so we create a placeholder
-    # for them using st.empty()
-    frame_text = st.sidebar.empty()
-    image = st.empty()
-
-    m, n, s = 960, 640, 400
-    x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
-    y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
-
-    for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
-        # Here were setting value for these two elements.
-        progress_bar.progress(frame_num)
-        frame_text.text("Frame %i/100" % (frame_num + 1))
-
-        # Performing some fractal wizardry.
-        c = separation * np.exp(1j * a)
-        Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
-        C = np.full((n, m), c)
-        M: Any = np.full((n, m), True, dtype=bool)
-        N = np.zeros((n, m))
-
-        for i in range(iterations):
-            Z[M] = Z[M] * Z[M] + C[M]
-            M[np.abs(Z) > 2] = False
-            N[M] = i
-
-        # Update the image placeholder by calling the image() function on it.
-        image.image(1.0 - (N / N.max()), use_column_width=True)
-
-    # We clear elements by calling empty on them.
-    progress_bar.empty()
-    frame_text.empty()
-
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
+    educbox = px.box(ulttest, x = "PEEDUCA", y = "Income", color = "PESEX")
+    educhist = px.histogram(sample2024, x = "PEEDUCA", barmode = "group", histnorm = "percent")
+    kidbar = px.bar(kiddata, x = "PEEDUCA", y = "PRNMCHLD")
+    childrenvinc = px.scatter(incvchld, x = "Income", y = "PRNMCHLD")
+    racebar = px.bar(raceinc, x = "PTDTRACE", y = "Income")
 
 
-st.set_page_config(page_title="Animation Demo", page_icon="📹")
-st.markdown("# Animation Demo")
-st.sidebar.header("Animation Demo")
-st.write(
-    """This app shows how you can use Streamlit to build cool animations.
-It displays an animated fractal based on the the Julia Set. Use the slider
-to tune different parameters."""
-)
+    ##Eventually edit to make this main data
+    fullData =data.HEFAMINC.str.extract(r'(\d+,?\d+)?([^0-9]*)?(\d+?,\d+)?')
+    fullData[0] = fullData[0].str.replace(',','')
+    fullData[0] = fullData[0].fillna("0")
+    fullData[0] = fullData[0].astype(int)
+    fullData = pd.concat([fullData,data], axis = 1)
+    fullData = fullData.rename(columns = {0:"Income"})
 
-animation_demo()
+    empl = fullData[['PEEDUCA','PREXPLF']]
+    empl = empl.groupby(['PREXPLF','PEEDUCA']).size().reset_index()
+    empl = empl.rename(columns = {0:"Count"})
+    empl = empl[empl["PREXPLF"] != "In Universe, Met No Conditions To Assign"]
+    emplbar = px.histogram(empl, x = "PREXPLF", y = "Count", color = "PEEDUCA", barmode = "group", histnorm = "percent")
 
-show_code(animation_demo)
+    #End of comment
+    
+    st.plotly_chart(emplbar)
+    st.plotly_chart(childrenvinc)
+    st.plotly_chart(kidbar)
+    st.plotly_chart(educhist)
+    st.plotly_chart(racebar)
 
 
-
+page2()
