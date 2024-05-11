f791185a917c23e5156bf270895ee5c2133f755e
JohnMacStar <157549072+JohnMacStar@users.noreply.github.com>
2024-05-11 15:49:53 -0500

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -28,34 +28,35 @@ def run():
     )
     st.write("# Introduction")
 
-data = pd.read_csv("https://github.com/JohnMacStar/semester-project-econ8320/releases/download/Data/ECON8320Final.csv")
-
-fullData =data.HEFAMINC.str.extract(r'(\d+,?\d+)?([^0-9]*)?(\d+?,\d+)?')
-fullData[0] = fullData[0].str.replace(',','')
-fullData[0] = fullData[0].fillna("0")
-fullData[0] = fullData[0].astype(int)
-fullData = pd.concat([fullData,data], axis = 1)
-fullData = fullData.rename(columns = {0:"Income"})
-fullData['Income'] = fullData['Income'].astype(int)
-fullData = fullData[['Income','STATE']]
-fullData = fullData.groupby("STATE").mean().reset_index()
-
-data = data[["STATE","PEEDUCA"]]
-data = data.groupby(["STATE","PEEDUCA"]).size().reset_index()
-data = data.rename(columns={0:"Count"})
-
-
-data['Count'] = data["Count"].astype(int)
-test = data.groupby(['STATE']).Count.sum().reset_index()
-data = pd.merge(data,test, on="STATE")
-data = data[data['PEEDUCA'] == "MASTER'S DEGREE(EX:MA,MS,MEng,MEd,MSW)"]
-data['total'] = (data['Count_x'] / data['Count_y'])*100
-
-mapdata = px.choropleth(locationmode = "USA-states", locations = data['STATE'], color = data['total'], scope = "usa", range_color=(10,23))
-incmap = px.choropleth(locationmode = "USA-states", locations = fullData['STATE'], color = fullData['Income'], scope = "usa")
+    data = pd.read_csv("https://github.com/JohnMacStar/semester-project-econ8320/releases/download/Data/ECON8320Final.csv")
+    
+    fullData =data.HEFAMINC.str.extract(r'(\d+,?\d+)?([^0-9]*)?(\d+?,\d+)?')
+    fullData[0] = fullData[0].str.replace(',','')
+    fullData[0] = fullData[0].fillna("0")
+    fullData[0] = fullData[0].astype(int)
+    fullData = pd.concat([fullData,data], axis = 1)
+    fullData = fullData.rename(columns = {0:"Income"})
+    fullData['Income'] = fullData['Income'].astype(int)
+    fullData = fullData[['Income','STATE']]
+    fullData = fullData.groupby("STATE").mean().reset_index()
+    
+    data = data[["STATE","PEEDUCA"]]
+    data = data.groupby(["STATE","PEEDUCA"]).size().reset_index()
+    data = data.rename(columns={0:"Count"})
     
     
+    data['Count'] = data["Count"].astype(int)
+    test = data.groupby(['STATE']).Count.sum().reset_index()
+    data = pd.merge(data,test, on="STATE")
+    data = data[data['PEEDUCA'] == "MASTER'S DEGREE(EX:MA,MS,MEng,MEd,MSW)"]
+    data['total'] = (data['Count_x'] / data['Count_y'])*100
+    
+    mapdata = px.choropleth(locationmode = "USA-states", locations = data['STATE'], color = data['total'], scope = "usa", range_color=(10,23))
+    incmap = px.choropleth(locationmode = "USA-states", locations = fullData['STATE'], color = fullData['Income'], scope = "usa")
+        
+        
     st.plotly_chart(mapdata)
     st.plotly_chart(incmap)
+    
 if __name__ == "__main__":
     run()
