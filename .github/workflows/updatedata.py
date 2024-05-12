53e26ebbf6ab7a045437091b1c93f8912d35e726
JohnMacStar <157549072+JohnMacStar@users.noreply.github.com>
2024-05-12 16:37:33 -0500

    Update updatedata.py

--- .github/workflows/updatedata.py
+++ .github/workflows/updatedata.py
@@ -103,13 +103,9 @@ df = df.reset_index(drop=True)
 
 data = df.replace({"STATE":statedict,"CBSA":citydict, "PEEDUCA":educationdict, "HEFAMINC":Incomedict,"PREXPLF":Unemployeddict,"PRHRUSL":Workweekdict,"PESEX":Sexdict,"PTDTRACE":Racedict,"PEMARITL":Martialdict})
 
-data = data.drop(df.columns[[0]], axis=1)
 del data['PRHRUSL']
-#data = data[(data['PEEDUCA'] == "Bachelor's Degree(ex:ba,ab,bs)") | (data['PEEDUCA'] == "MASTER'S DEGREE(EX:MA,MS,MEng,MEd,MSW)") | (data['PEEDUCA'] =="High School Grad-Diploma Or Equiv (ged)")]
-data = data.rename(columns=df.iloc[0])
-data = data.iloc[1:]
-data = data.reset_index(drop=True)
+data = data[(data['PEEDUCA'] == "Bachelor's Degree(ex:ba,ab,bs)") | (data['PEEDUCA'] == "MASTER'S DEGREE(EX:MA,MS,MEng,MEd,MSW)") | (data['PEEDUCA'] =="High School Grad-Diploma Or Equiv (ged)")]
 
 #data = data.rename(columns = {'Unnamed: 12':"Year"})
 
-data.to_csv("DataUpdate.csv")
+data.to_csv("DataUpdate.csv", header = False)
