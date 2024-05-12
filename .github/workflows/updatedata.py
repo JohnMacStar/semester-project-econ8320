ca9fb078a07eafcc6b0be6d0db3a97bbd4795424
JohnMacStar <157549072+JohnMacStar@users.noreply.github.com>
2024-05-12 15:35:24 -0500

    Update updatedata.py

--- .github/workflows/updatedata.py
+++ .github/workflows/updatedata.py
@@ -105,7 +105,7 @@ df = df.reset_index(drop=True)
 data = df.replace({"STATE":statedict,"CBSA":citydict, "PEEDUCA":educationdict, "HEFAMINC":Incomedict,"PREXPLF":Unemployeddict,"PRHRUSL":Workweekdict,"PESEX":Sexdict,"PTDTRACE":Racedict,"PEMARITL":Martialdict})
 
 #del data['PRHRUSL']
-data = data[(data['PEEDUCA'] == "Bachelor's Degree(ex:ba,ab,bs)") | (data['PEEDUCA'] == "MASTER'S DEGREE(EX:MA,MS,MEng,MEd,MSW)") | (data['PEEDUCA'] =="High School Grad-Diploma Or Equiv (ged)")]
+#data = data[(data['PEEDUCA'] == "Bachelor's Degree(ex:ba,ab,bs)") | (data['PEEDUCA'] == "MASTER'S DEGREE(EX:MA,MS,MEng,MEd,MSW)") | (data['PEEDUCA'] =="High School Grad-Diploma Or Equiv (ged)")]
 data = data.reset_index(drop = True)
 #data = data.rename(columns = {'Unnamed: 12':"Year"})
 
