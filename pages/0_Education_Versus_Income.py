479f3bd32aa84b9edf6c16b9e84e5f852cffe5e8
JohnMacStar <157549072+JohnMacStar@users.noreply.github.com>
2024-05-11 23:24:43 -0500

    Update 0_Education_Versus_Income.py

--- pages/0_Education_Versus_Income.py
+++ pages/0_Education_Versus_Income.py
@@ -49,7 +49,7 @@ def Page1():
     raceinc = raceinc.sort_values('Income', ascending=False)
     
     
-    educbox = px.box(ulttest, x = "PEEDUCA", y = "Income", color = "PESEX", labels={'PESEX':"Sex", "PEEDUCA": "Education Attained"}, color_discrete_sequence=["#00FFFF","#FF007F"])
+    educbox = px.box(ulttest, x = "PEEDUCA", y = "Income", color = "PESEX", labels={'PESEX':"Sex", "PEEDUCA": "Education Attained"}, color_discrete_sequence=["#00FFFF","#FF6EC7"])
     educhist = px.histogram(sample2024, x = "PEEDUCA", barmode = "group", histnorm = "percent", labels={'color':"Education Attained"})
     kidbar = px.bar(kiddata, x = "PEEDUCA", y = "PRNMCHLD", labels={'color':"Education Attained","PEERNHRO": "Education Attained"})
 
